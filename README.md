d72284cdcaac0320b6326476102a03fcd3523438
Hayden Offord <Hayden@Haydens-MacBook-Pro.local>
2021-08-27 18:37:41 +1000

    NOT FINISHED - Updated Track win and scoreboard, added popup to game start, fixed CSS styling

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # Tic-Tac-Toe
 First Project - Unit 1 GA
 
-First project creating Tic-Tac-Toe using HTML, CSS, and JS
-Simple version
+Web-based version of the classic 'Tic-Tac-Toe' game using HTML, CSS, and JS
 
-RuPaul inspired background
-Extensive CSS
+<h2>How to Play:</h2>
+Tic-tac-toe, noughts and crosses, or Xs and Os, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a diagonal, horizontal, or vertical row is the winner. It is a solved game with a forced draw assuming best play from both players. 
+<h6>Source: https://en.wikipedia.org/wiki/Tic-tac-toe</h6>
+
+HTML
+    - Simple header and content layout
+    - Scoreboard using grid template
+    - 
+CSS
     - Created board using CSS
     - Created 'X' and 'O' using raw css
+    - Created Draw/Win message
 
-Simple JS
+JS
     - Track possible winning combos using variable array
     - Click handles and event listeners linked to hover class and on-clicks
     - Check draw/win and display draw/win message, with a restart game option
+    - Track Game wins and assign new value to appropriate player
+    - Popup Game start message on page load
+
+Tutorial used to build base code:
+https://www.youtube.com/watch?v=Y-GkMjUZsmM&t=2201s
 
 Intend to add the following extensions:
 <!-- EXTENSIONS -->
-<!-- Rules page/section -->
-<!-- A before startGame gif message pop-up of RuPaul saying "Don't Fuck It Up -->
+<!-- VS computer AI -->
+<!-- Play as RuPaul All Stars queens instead of X's and O's -->
 <!-- Computer difficulty in Single Player selector -->
 <!-- Ability to input Player names -->
-<!-- Track player scores -->
--- ttt.js
+++ ttt.js
@@ -11,38 +11,80 @@ const winCombo = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+
 const cellEl = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
-const winMsgEl = document.getElementById('winMsg')
+
+const startMsgEl = document.getElementById('startMsg')
+const startBtn = document.getElementById('startBtn')
+
+const winMsgEl = window.document.getElementById('winMsg')
 const restartBtn = document.getElementById('restartBtn')
 const winMsgTextEl = document.querySelector('[data-win-msg-text]')
+
+const xScoreResult = document.getElementById('xScore')
+const oScoreResult = document.getElementById('oScore')
 
 // define a player variable to use boolean values in functions to define player turn
 let oTurn
 
+// let currentPlayer = ''
+
+// ------------------------------------------ //
+
+
+// Start game mesage popup on first page load
+
+window.addEventListener('load', function() {
+  startMessage();
+})
+
+function startMessage() {
+  startMsgEl.classList.add('show');
+}
+
+startBtn.addEventListener('click', function() {
+  startMsgEl.classList.remove('show')
+})
+
+// function closeWindow() {
+//   startMsgEl.modal('show')
+//   startBtn.addEventListener('click')
+//   startMsgEl.classList.remove('show')
+// }
+// startBtn.addEventListener('click', startGame)
+
+// ------------------------------------------ //
 startGame()
 
 // link Restart to startGame function
+
 restartBtn.addEventListener('click', startGame)
 
+// ------------------------------------------ //
+
+// Start game function
 function startGame() {
   oTurn = false
   cellEl.forEach(cell => {
     cell.classList.remove(xClass)
     cell.classList.remove(oClass)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
+    cell.removeEventListener('click', ifClicked)
+    cell.addEventListener('click', ifClicked, { once: true })
   })
   setBoardHoverClass()
   winMsgEl.classList.remove('show')
 }
 
-function handleClick(e) {
-  const cell = e.target
+// Main gamplay code
+function ifClicked(event) {
+  const cell = event.target
   const currentClass = oTurn ? oClass : xClass
   placeMark(cell, currentClass)
   if (checkWin(currentClass)) {
     endGame(false)
+    scoreResult()
   } else if (isDraw()) {
     endGame(true)
   } else {
@@ -54,13 +96,26 @@ function handleClick(e) {
 // Win/Draw message
 function endGame(draw) {
   if (draw) {
-    winMsgTextEl.innerText = 'Draw!'
+    winMsgTextEl.innerText = "It's a tie baby!"
   } else {
-    winMsgTextEl.innerText = `${oTurn ? "O's" : "X's"} Wins!`
+    winMsgTextEl.innerText = `${oTurn ? "O" : "X"} ... You're a winner baby!`
   }
   winMsgEl.classList.add('show')
 }
 
+// Track score
+function scoreResult() {
+  const currentClass = oTurn ? oClass : xClass
+  if ('x' === currentClass) {
+    let currentXScore = Number(xScoreResult.innerText)
+    xScoreResult.innerHTML = currentXScore + 1
+  } else if ('o' === currentClass) {
+    let currentOScore = Number(oScoreResult.innerText)
+    oScoreResult.innerHTML = currentOScore + 1
+  }
+}
+
+// ------------------------------------------ //
 
 // functions to call in main code
 function isDraw() {
@@ -88,7 +143,7 @@ function setBoardHoverClass() {
 }
 
 function checkWin(currentClass) {
-    // .some determines whether the specified callback => returs 'true' boolean for any element in the array
+    // .some determines whether the specified callback => returns 'true' boolean for any element in the array
   return winCombo.some(combination => {
       // .every determines whether all members of an array satisfy the test (above)
     return combination.every(index => {
